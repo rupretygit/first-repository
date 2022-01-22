e970b7d2014f1a2ca419c951376c896bb23bd597
upret <upret@LAPTOP-RTC24KA1>
2022-01-22 14:27:05 -0600

    4th

--- GitConnection/src/demo/Demo.java
+++ GitConnection/src/demo/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Test is successful");
-		System.out.println("2nd test is successful");
+		System.out.println("4th test is successful");
 
 	}
 
